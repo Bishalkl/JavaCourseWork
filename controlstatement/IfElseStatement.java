d39a54e261aa5487da520befad78605686e3cce5
Bishalkl <bishal123koirala@gmail.com>
2023-09-04 13:14:09 +0545

    Done today

---
+++ controlstatement/IfElseStatement.java
@@ -0,0 +1,18 @@
+package controlstatement;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        // Test
+        System.out.println("Hello world");
+
+        //Example 
+        int a = 20;
+
+        if (a == 20){
+            System.out.println('Y');
+        }
+        else{
+            System.out.println('N');
+        }
+    }
+}
