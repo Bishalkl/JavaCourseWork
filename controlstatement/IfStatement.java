7b9c888a88206f664dd227a546943942bf1a7a05
Bishalkl <bishal123koirala@gmail.com>
2023-09-04 12:50:22 +0545

    Initial commit

---
+++ controlstatement/IfStatement.java
@@ -0,0 +1,20 @@
+
+// package name always should be in lowercase
+
+package controlstatement;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        // Test 
+        System.out.println("Hello world");
+
+        // Example IF STATEMENT ONLY
+        int a = 12 , b = 13;
+
+        if (a < b){
+            System.out.println('Y');
+        }
+
+    }    
+}
+
