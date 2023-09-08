153f05868846d6a260d380044bd3e23a23850a05
Bishalkl <bishal123koirala@gmail.com>
2023-09-08 20:00:49 +0545

    adding basics
    learning java

---
+++ TestProgram.java
@@ -0,0 +1,7 @@
+public class TestProgram {
+    public static void main(String[] args) {
+        // output
+       System.out.println("*\n**\n***\n****");
+
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Test
+        System.out.println("variable");
+        
+        // Variable
+        int a = 12;
+        int b = 13;
+
+        String c= "bishal"
+
+    }
+}
