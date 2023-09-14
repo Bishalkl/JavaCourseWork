da94accb737b6f60e5f70db00ebf9496292399b8
Bishalkl <bishal123koirala@gmail.com>
2023-09-14 20:43:56 +0545

    done
    learned loop in java

---
+++ loopJava.java
@@ -0,0 +1,22 @@
+public class loopJava {
+    public static void main(String[] args) {
+         //for loop syntax and test
+         for(int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+        
+        //while loop syntax and test
+        int a = 1;
+        while(a < 11){
+            System.out.println("Hello world");
+            a++;
+        }
+        
+        // do while loop syntax and test
+        int b = 1;
+        do{
+            System.out.println(b);
+            b++;
+        }while( b < 11);
+    }
+}
