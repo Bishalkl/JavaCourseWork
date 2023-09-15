2ccfc4554f57086c406d4642c8a028100c69abe4
Bishalkl <bishal123koirala@gmail.com>
2023-09-15 17:14:28 +0545

    done
    done

--- MenuDriven.java
+++ MenuDriven.java
@@ -3,7 +3,6 @@ public class MenuDriven {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int i = 1;
 
        if (n == 1){
         int marks;
---
+++ pattern/FirstPattern.java
@@ -0,0 +1,13 @@
+package pattern;
+
+public class FirstPattern {
+    public static void main(String[] args) {
+        
+        for( int column=0; column<4; column++){
+            for(int row=0; row<5; row++){
+                System.out.print('*');
+            }
+             System.out.println();
+        }
+    }
+}
