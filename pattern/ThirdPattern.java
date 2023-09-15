0f4b3fd839a8f1774ef9dd62da73bab79da3acf5
Bishalkl <bishal123koirala@gmail.com>
2023-09-15 19:17:11 +0545

    done
    done little confusing

--- pattern/ThirdPattern.java
+++ pattern/ThirdPattern.java
@@ -2,11 +2,25 @@ package pattern;
 public class ThirdPattern {
     public static void main(String[] args) {
         int r = 4;
-        for(int i=1; i<=r; i++){
-            for(int j=1; j<=i; j++){
+        // outerloop
+        for(int i=1; i<=4; i++){  
+            // inner loop for space
+            for(int j=1; j<= r-1; j++){
+                System.out.print(" ");
+            }
+
+            //inner loop for *
+            for(int k=1; k<=i; k++){
                 System.out.print("*");
             }
+            r--;
             System.out.println();
         }
     }
 }
+// ---*
+// --**
+// -***
+// ****
+
+  
