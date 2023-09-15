3eba6d89afdd1b833123eefe939d710a79bdaa42
Bishalkl <bishal123koirala@gmail.com>
2023-09-15 17:53:10 +0545

    done
    done

--- pattern/ThirdPattern.java
+++ pattern/ThirdPattern.java
@@ -2,8 +2,6 @@ package pattern;
 public class ThirdPattern {
     public static void main(String[] args) {
         int r = 4;
-        int c = 5;
-
         for(int i=1; i<=r; i++){
             for(int j=1; j<=i; j++){
                 System.out.print("*");
