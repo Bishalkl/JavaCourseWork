cc1bb87c0f52edd1980a500c678fa34de92c38df
Bishalkl <bishal123koirala@gmail.com>
2023-09-16 22:40:44 +0545

    done
    done

--- pattern/FifthPattern.java
+++ pattern/FifthPattern.java
@@ -2,11 +2,13 @@ package pattern;
 
 public class FifthPattern {
     public static void main(String[] args) {
-        int f = 5;
-        for(int i=1; i<=5; i++){
-            for(int j = 1; j<=f-i+1; j++){
+        int c = 5;
+        int r = 5;
+        for(int i=1; i<=r; i++){
+            for(int j = 1; j<=c; j++){
                 System.out.print(j);
             }
+            c--;
             System.out.println();
         }
     }
