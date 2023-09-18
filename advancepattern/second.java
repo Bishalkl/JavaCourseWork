46d709c3dafd8760300fce978aba9a9f3ecb2b69
Bishalkl <bishal123koirala@gmail.com>
2023-09-18 12:25:32 +0545

    done
    done

--- advancepattern/second.java
+++ advancepattern/second.java
@@ -3,10 +3,13 @@ package advancepattern;
 public class second {
     public static void main(String[] args) {
          int n =4;
+        //  outer loop for row 
          for(int i=n; i>=1; i--){
+            // inner loop for space
             for(int j=1; j<=i; j++){
                 System.out.print(" ");
             }
+            // innerloop for star
             for(int j=1; j<=n+1; j++){
                 System.out.print("*");
             }
---
+++ advancepattern/third.java
@@ -0,0 +1,19 @@
+package advancepattern;
+
+public class third {
+    public static void main(String[] args) {
+        int n =1;
+        // outer loop for row
+        for(int i=1; i<=5; i++){
+
+            // inner loop for column
+            for(int j=1; j<=i; j++){
+                System.out.print(n + " ");
+                
+            }
+            System.out.println();
+            n++;
+            
+        }
+    }
+}
