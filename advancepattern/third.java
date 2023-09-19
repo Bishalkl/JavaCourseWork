6030e2fe0c52c3c0ba6943c1153965a9088c242f
Bishalkl <bishal123koirala@gmail.com>
2023-09-19 15:01:38 +0545

    done
    done

--- advancepattern/third.java
+++ advancepattern/third.java
@@ -5,10 +5,14 @@ public class third {
         int n =1;
         // outer loop for row
         for(int i=1; i<=5; i++){
+            // inner loop for space loop 
+            for(int j=5; j>=i; j--){
+                System.out.print(" ");
+            }
 
             // inner loop for column
             for(int j=1; j<=i; j++){
-                System.out.print(n + " ");
+                System.out.print(n +" ");
                 
             }
             System.out.println();
