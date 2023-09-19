83ee0e075db6894559d9b45c406c0a0746bbe99d
Bishalkl <bishal123koirala@gmail.com>
2023-09-19 21:30:54 +0545

    done
     finished loop

--- advancepattern/fifth.java
+++ advancepattern/fifth.java
@@ -2,28 +2,30 @@ package advancepattern;
 
 public class fifth {
     public static void main(String[] args) {
-        int n =5;
-        // First half
-        //  outer loop
-        for(int i=1; i<=5; i++){
-            // inner loop for column
+        int n = 4;
+
+        // first half
+        for(int i=1; i<=n; i++){
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            // inner loop for star
             for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
-        //  Second half
-        //  outer loop
-        for( int i=n; i>=1; i--){
-             for(int j=1; j<=n-i; j++){
+        // second half
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            // inner loop for star
             for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=1; j<=i-1; j++){
                 System.out.print("*");
             }
             System.out.println();
